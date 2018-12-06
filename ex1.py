3bfd6cf73ebf0006e9d27a730094ab46d6ecab04
Vinicius Machado <viniciusmach07@gmail.com>
2018-12-06 01:49:50 -0200

    campo desenhado

--- ex1.py
+++ ex1.py
@@ -3,6 +3,8 @@ import pygame
 branco = (255, 255, 255)
 preto = (0, 0, 0)
 vermelho = (255, 0, 0)
+vermei = (134, 28,9)
+amarelo = (212, 169, 65)
 verde = (0, 255, 0)
 azul = (0, 0, 255)
 
@@ -11,8 +13,8 @@ try:
 except:
     print("O modulo pygame não foi inicializado com suças")
 
-largura = 640
-altura = 480    
+largura = 970
+altura = 640   
 tamanho = 10
 pos_x = largura/2
 pos_y = altura/2
@@ -26,9 +28,40 @@ while sair:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sair = False
-    fundo.fill(branco)
-    pygame.draw.rect(fundo, preto, [pos_x, pos_y, tamanho, tamanho])
-    #pos_x += 0.1
+    fundo.fill(vermei)
+    
+    #BORDAS / CAMPO
+    pygame.draw.rect(fundo, amarelo, [0, 50, largura, 20])
+    pygame.draw.rect(fundo, amarelo, [0, 50, 20, altura])
+    pygame.draw.rect(fundo, amarelo, [largura - 20, 50, 20, altura])
+    pygame.draw.rect(fundo, amarelo, [0, altura - 20, largura, 20])
+
+    pygame.draw.rect(fundo, amarelo, [largura/2 - 25, 70, 50, 50])
+    pygame.draw.rect(fundo, amarelo, [largura/2 - 25, altura - 70, 50, 50])
+
+    pygame.draw.rect(fundo, amarelo, [120, 150, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 190, 150, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [120, altura - 120, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 190, altura - 120, 70, 20])
+
+    pygame.draw.rect(fundo, amarelo, [100, 240, 20, 20])
+    pygame.draw.rect(fundo, amarelo, [100, 430, 20, 20])
+    pygame.draw.rect(fundo, amarelo, [120, 240, 25, 210])
+    pygame.draw.rect(fundo, amarelo, [largura - 120, 240, 20, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 120, 430, 20, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 145, 240, 25, 210])
+
+    pygame.draw.rect(fundo, amarelo, [250, altura/2, 50, 50])
+    pygame.draw.rect(fundo, amarelo, [largura - 295, altura/2, 50, 50])
+
+    pygame.draw.rect(fundo, amarelo, [350, 200, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [350, 220, 20, 25])
+    pygame.draw.rect(fundo, amarelo, [largura - 420, 200, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 370, 220, 20, 25])
+    pygame.draw.rect(fundo, amarelo, [350, altura - 180, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [350, altura - 200, 20, 25])
+    pygame.draw.rect(fundo, amarelo, [largura - 420, altura - 180, 70, 20])
+    pygame.draw.rect(fundo, amarelo, [largura - 370, altura - 200, 20, 25])
 
     pygame.display.update()
 
